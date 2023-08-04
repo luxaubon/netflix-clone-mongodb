c3960c9acfdffe7ae0687c6d79a6157bf11345eb
Luxaubon <>
2023-08-04 15:14:56 +0700

    first commit

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -11,14 +11,14 @@ export default function MovieList({data,title} : MovieListProps) {
     if(isEmpty(data)){
         return false;
     }
-
+    console.log(data);
    return (
     <div className="px-4 md:px-12 mt-4 space-y-8">
       <div>
         <p className="text-white text-md md:text-xl lg:text-2xl font-semibold mb-4">{title}</p>
         <div className="grid grid-cols-4 gap-2">
           {data.map((movie) => (
-                <MovieCard key={movie.id} data={movie} />
+                <MovieCard key={movie.id} url={movie.thumbnailUrl} id={movie.id} title={movie.title} duration={movie.duration}/>
           ))}
         </div>
       </div>
